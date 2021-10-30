3b5f375f8e7bc4a8f35295d19a25359bf1509984
vinnyciusf5 <vinnyciusf5@hotmail.com>
2021-10-29 23:52:54 -0300

    first commit

---
+++ catalogoscript.js
@@ -0,0 +1,18 @@
+function insta(){
+    window.open('https://www.instagram.com/hotcosmeticsvca/')
+}
+function catalogo(){
+    window.location.href='catalogo.html'
+}
+function zap(){
+    window.open('https://api.whatsapp.com/send?phone=5577997014456')
+}
+function tt(){
+    window.open('https://www.tiktok.com/@hotcosmeticsvca')
+}
+function fb(){
+    window.open('https://www.facebook.com/hotcosmeticsvca')
+}
+function blogo(){
+    window.location.href='catalogo.html'
+}
